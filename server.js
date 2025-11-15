24c15f1dcbd175bbdc024242b222ea9ab9c45a58
RooopeshKumarNani <roopesh.kandanuru.nani@gmail.com>
2025-11-15 21:44:03 +0530

    Fix CORS: use request origin instead of wildcard with credentials

--- server.js
+++ server.js
@@ -97,7 +97,8 @@ app.use(express.json());
 
 // Enable CORS for cross-origin requests (Firebase to Render)
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  const origin = req.headers.origin || "*";
+  res.header("Access-Control-Allow-Origin", origin);
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, x-invite-token");
   res.header("Access-Control-Allow-Credentials", "true");
