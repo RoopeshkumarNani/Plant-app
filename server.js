5b771dd75ff1752594614290dd09ac92ba01184e
RoopeshkumarNani <roopesh.kandanuru.nani@gmail.com>
2025-11-27 00:00:33 +0530

    Fix CORS error: Allow Expires header

--- server.js
+++ server.js
@@ -492,7 +492,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header(
     "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, x-invite-token"
+    "Content-Type, Authorization, x-invite-token, Expires, Pragma, Cache-Control"
   );
   res.header("Access-Control-Allow-Credentials", "true");
 
